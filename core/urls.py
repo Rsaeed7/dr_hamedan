96b79fd17117fcf6bb13fae1d8b0abbd87aa8510
Saeed <saye00n@gmail.com>
2025-02-25 20:22:46 +0330

    add special list

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home, name='homepage'),
     path('clinics', views.clinic_list, name='clinic_list'),
+    path('special_list', views.special_list, name='special_list'),
     path('clinic_detail', views.clinic_detail, name='clinic_detail'),
     path('article_list', views.article_list, name='article_list'),
     path('article_detail', views.article_detail, name='article_detail'),
