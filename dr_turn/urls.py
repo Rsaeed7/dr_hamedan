1f6a47ea760ed81572d963614f954d8aaa775f3d
siavash rahimi <rahimi.f.h@gmail.com>
2025-04-24 11:42:14 +0330

    account app

--- dr_turn/urls.py
+++ dr_turn/urls.py
@@ -31,10 +31,8 @@ urlpatterns = [
     path('', include('doctors.urls', namespace='home')),  # Home page will be handled by doctors app
     
     # Authentication URLs
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='doctors:doctor_list', http_method_names=['get', 'post']), name='logout'),
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='auth/password_change.html'), name='password_change'),
-    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='auth/password_change_done.html'), name='password_change_done'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
 
 if settings.DEBUG:
