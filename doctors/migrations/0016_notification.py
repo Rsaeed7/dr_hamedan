f54435fd7cb7542f432ce7b1ee55751d20560da7
siavash rahimi <rahimi.f.h@gmail.com>
2025-07-11 15:52:16 +0330

    fix

--- doctors/migrations/0016_notification.py
+++ doctors/migrations/0016_notification.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('doctors', '0015_doctorblockedday'),
+        ('doctors', '0014_emailtemplate'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
